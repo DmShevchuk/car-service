4de0bebe0dee636047912b523ba340bf0c69044e
DmShevchuk <dmitriishevchuk2003@yandex.ru>
2022-08-06 18:16:14 +0300

    Add exception handling strategy

--- src/main/java/com/example/carservice/controllers/ExceptionController.java
+++ src/main/java/com/example/carservice/controllers/ExceptionController.java
@@ -1,15 +1,17 @@
 package com.example.carservice.controllers;
 
+import com.example.carservice.entities.exception.ApiError;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+
+@RestControllerAdvice
 public class ExceptionController {
-//    @ExceptionHandler(Exception.class)
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    public String handleException(Exception e) {
-//        return e.getMessage();
-//    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleException(Exception e) {
+        return new ApiError(e.getClass().getName(), e.getMessage());
+    }
 }
